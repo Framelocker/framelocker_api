a8637ef815eb0fa6728541d399a8bfb1e51fba97
Vazcore <vazcore@live.ru>
2015-04-08 17:31:22 +0300

    API NodeJS docs

---
+++ docs/chat_invitation.md
@@ -0,0 +1,161 @@
+# NodeJS Chat
+
+## 
+
+### Socket connection
+
+ *  Enable socket script to site where API will be used:
+
+```javascript
+<script type="text/javascript" src="http://chat.framelocker.com:8081/socket.io/socket.io.js"></script> 
+```
+> - [Domain] - http://chat.framelocker.com - Domain where NodeJS server installed
+
+ * Obtain token with authorization method [`signin`](http://vk.com) using server side API:
+ 
+```javascript
+	var token = data.token;		 
+```
+
+* Create socket object (using token):
+
+```javascript
+	var socket = io('chat.framelocker.com:8081/api?token=[token]');
+``` 
+### Connecting to room
+* You should specify the room (rooms):
+
+```javascript	
+  var room = "test_room"; (`example`)	
+```
+
+* For joining to that room send message to socket(join_room):
+
+```javascript	
+  socket.emit('join_room', {room:room});
+```
+
+* We can listen log of our activity("notifications"):
+
+```javascript	
+	socket.on('notifications', function(data){
+		$("#responses").html("<p>Status: "+data.status+". "+data.description+"</p>");
+	});	
+```	
+
+>  - In response JSON we can get "status" parameter and "description" parameter so we can react to.
+
+> - Also response data contains "request_type" field it helps defined response type. For now API supports request_type with values: "operation" and "invite"
+
+### Send Message
+
+* To handle with "sending messages" operation just send message to socket(send_message) using token and room:
+> - For example we want to send some text from "keypress" event
+
+
+```javascript	
+	$("#messager input").keypress(function(e){
+		if(e.which == 13){
+			var message = $(this).val();
+			socket.emit("send_message", {room:room, msg:message});
+		}
+	});	
+```
+
+### Listen for recieved messages
+
+* Also we should listen and wait for new messages in our room
+
+```javascript	
+	socket.on('get_messages', function(data){
+		var message_display = $("#messager ul");
+		$.each(data, function(i, val){
+			message_display.append("<li><p><img [src]='"+val.avatar+"'></p><p>"+val.name+"</p><p>"+val.msg+"</p></li>");			
+		});
+	});	
+```
+
+>  - Our response data is array of messages(objects)
+
+>  - If we will join multiple rooms we should sort our response data, so check val.path that contains room;
+
+### Invites (General)
+
+* It is possible to invite people with `room` or without.
+* In case 2 - API will generate room name for you
+* Using static room name will help us to catch previous message history
+
+### Invite to Single User
+
+* Send message to socket for triggering invitation event `invite_to_chat`
+
+```javascript	
+	var uid = //;
+	socket.emit('invite_to_chat', {uid:uid});	
+```
+
+> - On this step we should identify the user we want to speak with 
+
+* Users should listen for possible invitations so developer who uses API should listen and catch notifications messages `notifications`
+
+```javascript	
+	socket.on('notifications', function(data){
+		// For now only invite to chat
+		if(data.request_type == 'invite'){
+			// So we need to react for such invitation
+		}else if(data.request_type == 'operation'){
+			// This data is just responses of different operations (display - optional)
+		}
+	});	
+```
+
+>  - It is important to understand and handle the type of response notifications!
+
+> - "request_type" for now contains only 2 value "invite" or "operations"
+
+>  - Developers should handle invite case and notify users with some Prompt window or Confirm, or ...
+
+* On this step user received a notification for inviting. So after he'll accept it we should send a message about successful accepting `accept_invitaion`
+
+```javascript		
+		...
+		if(data.request_type == 'invite'){
+			var reponse = confirm('You have recieved an invitation from  '+data.name + ". Accept?");
+			if(reponse){
+				// Accept invitation
+				socket.emit('accept_invitaion', {room: data.room}); // 
+			}
+		}
+		...		
+```
+
+>  - Response data contains field - "room" - `data.room`, which we will use for sending messages
+
+* Handling accepting invitation Event:
+
+```javascript
+socket.on('notifications', function(data){
+  if(data.type == "accepted_invitation"){
+      $("#panelGeneral").addClass("hide");
+      $("#panelPM").removeClass("hide");
+      var udata = data.params.userData;	
+      ...
+```
+> - Data of `notifications` holds User Data `data.params.userData` and room name `data.params.room`
+
+* The form of sending messages is still the same
+
+```javascript	
+	...
+	socket.emit("send_message", {room:room, msg:message});
+	...	
+```
+
+### Group Invitation
+
+* For group invite just define array of user IDs and use `invite_to_chat`:
+
+```javascript
+var uids = [id1, id2 ... idn];
+socket.emit('invite_to_chat', {uid:uids});
+```
---
+++ examples/group_chat_invite/user1.php
@@ -0,0 +1,168 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Chat</title>
+	<link rel="stylesheet" type="text/css" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
+	<script type="text/javascript" src="js/jquery.js"></script>
+	<script type="text/javascript" src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+	<script type="text/javascript" src="http://localhost:8081/socket.io/socket.io.js"></script>
+</head>
+<body>
+
+<div class="container">	
+	<div class="panel panel-info" id="panelGeneral">
+		<div class="panel-heading">
+			<div class="input-group">
+	           <input type="text" id="chat_bar" class="form-control" placeholder="Enter Message">
+	            <span class="input-group-btn">
+	                <button class="btn btn-info" id="send" type="button">SEND</button>
+	            </span>
+	        </div>
+        </div>        
+        <div class="panel-body"><button class="btn btn-success" id="inv">Invite</button></div>
+        <div class="panel-body">        	
+        	<div class="media-list">
+        		
+        	</div>
+        </div>
+	</div>	
+</div>
+
+<div class="container">
+	<div class="row">
+		<button class="btn btn-warning" id="checkUsers">Check users</button>
+	</div>
+    <div class="row">
+        <div class="col-md-5">
+            <div class="panel panel-primary hide" id="panelPM">
+                <div class="panel-heading">
+                    <span class="glyphicon glyphicon-comment"></span> Chat
+                    <div class="btn-group pull-right">
+                        <button type="button" class="btn btn-default btn-xs dropdown-toggle" data-toggle="dropdown">
+                            <span class="glyphicon glyphicon-chevron-down"></span>
+                        </button>
+                        
+                    </div>
+                </div>
+                <div class="panel-body">
+                    <ul class="chat" style="list-style:none;">
+                        <li class="left clearfix chat_el hide"><span class="chat-img pull-left">
+                            <img src="#" alt="User Avatar" class="img-circle">
+                        </span>
+                            <div class="chat-body clearfix">
+                                <div class="header">
+                                    <strong class="primary-font pm_name"></strong> 
+                                    <small class="pull-right text-muted">
+                                        <span class="glyphicon glyphicon-time"></span>12 mins ago
+                                    </small>
+                                </div>
+                                <p class="pm_msg">
+                                    
+                                </p>
+                            </div>
+                        </li>                      
+                        
+                    </ul>
+                </div>
+                <div class="panel-footer">
+                    <div class="input-group">
+                        <input id="btn-input" type="text" class="form-control input-sm" placeholder="Type your message here...">
+                        <span class="input-group-btn">
+                            <button class="btn btn-warning btn-sm" id="btn-chat">
+                                Send</button>
+                        </span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+<li class="media hide">
+	<div class="media-body">
+		<div class="media">
+            <a class="pull-left" href="#"><img width="50px" class="media-object img-circle " src=""></a>
+            <div class="media-body"><span class="chat_msg">[MSG]</span><br><small class="text-muted"><span class="chat_name">[MSG]</span> | 23rd June at 5:00pm</small><hr></div>
+        </div>
+    </div>
+</li>
+
+<script type="text/javascript">
+	(function() {
+		var socket = io('http://localhost:8081/api?token=laganna-emperror');
+		var el = $(".media.hide");
+		var pm_el = $(".chat_el");
+		if(socket === undefined)
+			return false;	
+		
+		$("#inv").click(function() {
+			socket.emit('invite_to_chat', {uid:[23,49], room:"hatiko"});
+		});	
+
+
+		socket.on('notifications', function(data){
+			console.log(data);
+		    if(data.type == 'login' && data.status == 1){
+		    	$(document).trigger("success_login");
+		    }
+
+		    if(data.request_type == 'invite'){
+		        var reponse = confirm('You have recieved an invitation from  '+data.name + ". Accept? Room "+data.room);
+		        if(reponse){
+		            // Accept invitation
+		            socket.emit('accept_invitation', {room: data.room}); // 
+		        }
+		    }
+
+		    if(data.type == "accepted_invitation"){
+		    	$("#panelGeneral").addClass("hide");
+		    	$("#panelPM").removeClass("hide");
+		    	var udata = data.params.userData;
+
+		    	$("#btn-chat").click(function() {
+					var val = $(this).closest(".input-group").find("input").val();
+					console.log(data);					
+					socket.emit("send_message", {room:data.params.room, msg:val});
+				});
+
+				$("#checkUsers").click(function() {
+					socket.emit('get_room_users', {room: data.params.room}); 
+				});			    	
+
+		    }	
+		});
+
+		$(document).on("success_login", function() {
+			socket.emit('join_room', {room:"baltazor"});
+		});
+
+		$("#send").click(function() {
+			var msg = $("#chat_bar").val();
+			socket.emit('send_message', {room:"baltazor", msg:msg});
+		});
+
+		$("#chat_bar").keypress(function(e){
+			if(e.which == 13){
+				var msg = $("#chat_bar").val();
+				socket.emit('send_message', {room:"baltazor", msg:msg});	
+			}
+		});
+
+		socket.on('get_messages', function(data){
+		    var message_display = $(".media-list");
+		    $.each(data, function(i, val){
+		        var chat = el.clone();
+		        chat.removeClass("hide");
+		        chat.find(".chat_msg").text(val.msg);
+		        chat.find("img").attr("src", val.avatar);
+		        chat.find(".chat_name").text(val.name);
+		        message_display.append(chat);            
+		    });
+		});
+
+	})()
+	
+</script>
+</body>
+</html>
